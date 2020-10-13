0bfd84c040646b372705472d2a2077a081d06811
Witsudi Anasrullah <witsudi217@gmail.com>
2020-10-13 09:25:07 +0700

    update for build and add comment line

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'; //import file material from flutter package
 
 void main() => runApp(Home());
 
@@ -6,31 +6,77 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'my app',
-      debugShowCheckedModeBanner: false,
+      //menggunakan thema materialapp
+      title: 'Tugas 1 MOBILE', //
+      debugShowCheckedModeBanner: false, //menghilangkan banner debug
       home: Scaffold(
-        appBar: AppBar(title: Text('Infrastruktur, Industri dan Inovasi 9')),
+        //menggunakan widget scaffold
+        appBar: AppBar(
+            title: Text(
+                'Infrastruktur, Industri dan Inovasi (9)')), //menambahkan judul di appBar
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          //widget column
+          mainAxisAlignment: MainAxisAlignment
+              .center, //memposisikan children column ditengah layar
           children: [
             Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+              //widget row
+              mainAxisAlignment: MainAxisAlignment
+                  .center, //memposisikan children row ditengah layar
               children: [
                 Padding(
-                  padding: EdgeInsets.all(15),
-                  child: Text("Witsudi anasrullah"),
+                  padding: EdgeInsets.only(
+                      bottom: 20), //memberi padding dibawah saja pada text
+                  child: Text("Nama Anggota :"), //widget text
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment
+                  .center, //memposisikan children row ditengah layar
+              children: [
+                Padding(
+                  padding: EdgeInsets.fromLTRB(0, 0, 20,
+                      10), //memberikan padding pada text (left,top,right,bottom)
+                  child: Text("Witsudi Anasrullah"),
                 ),
                 Padding(
-                  padding: EdgeInsets.all(15),
+                  padding: EdgeInsets.fromLTRB(20, 0, 0,
+                      10), //memberikan padding pada text (left,top,right,bottom)
                   child: Text("182410103062"),
                 )
               ],
             ),
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              mainAxisAlignment: MainAxisAlignment
+                  .center, //memposisikan children row ditengah layar
               children: [
-                Text("Azizah"),
-                Text("182410103045"),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(0, 10, 10,
+                      10), //memberikan padding pada text (left,top,right,bottom)
+                  child: Text("Azizah Nurul Firdausi A."),
+                ),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(5, 10, 0,
+                      10), //memberikan padding pada text (left,top,right,bottom)
+                  child: Text("182410103042"),
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment
+                  .center, //memposisikan children row ditengah layar
+              children: [
+                Padding(
+                  padding: EdgeInsets.fromLTRB(0, 10, 40,
+                      0), //memberikan padding pada text (left,top,right,bottom)
+                  child: Text("Fatthur Rozi"),
+                ),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(40, 10, 0,
+                      0), //memberikan padding pada text (left,top,right,bottom)
+                  child: Text("182410103058"),
+                )
               ],
             )
           ],
